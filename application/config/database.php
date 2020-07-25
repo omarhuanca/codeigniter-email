d55d85454a4a2be66c056c08127c162233d1b7d5
omarhuanca <oma378501@yahoo.com>
2020-07-24 20:21:09 -0400

    add credential database pgsql

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
+	'dsn'	=> 'pgsql:host=ec2-184-72-235-80.compute-1.amazonaws.com;port=5432user=jhhyhfnaytxunk;password=cfd7313822a9b94fd7497c36f1de0bc98428717cdfd7d1ad7ca2849215e330e2;dbname=d5f7kfnltemnif',
+	'hostname' => '',
+	'username' => 'NULL',
+	'password' => 'NULL',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
